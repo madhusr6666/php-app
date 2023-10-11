ec2f95214570874a925c36c55f6a1b7759f7faaa
vivekvicky545 <131896557+vivekvicky545@users.noreply.github.com>
2023-10-11 18:35:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database connection details
 $db_host = 'database-2.cjway36ptgk6.us-east-1.rds.amazonaws.com';
-$db_name = 'database-2';
+$db_name = 'vivek-db';
 $db_user = 'admin';
 $db_pass = 'vivek123';
 ?>
