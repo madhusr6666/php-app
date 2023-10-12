94cca8ceb4412b4139e752878d9fbfccd6fa28d1
vivekvicky545 <131896557+vivekvicky545@users.noreply.github.com>
2023-10-12 12:34:00 +0530

    added correct db name

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database connection details
 $db_host = 'database-2.cjway36ptgk6.us-east-1.rds.amazonaws.com';
-$db_name = 'database-2';
+$db_name = 'vivekdb';
 $db_user = 'admin';
 $db_pass = 'vivek123';
 ?>
